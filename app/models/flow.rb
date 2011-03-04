71b4341bfa5f246e9ca50ca7c18a0c4e9ac0439a
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-04 12:54:23 -0300

    Small refactor in flow

--- app/models/flow.rb
+++ app/models/flow.rb
@@ -7,12 +7,12 @@ class Flow
     commands.each do |cmd|
       if cmd.is_a? Hash
         cmd, args = cmd.first
-        cmd = "#{cmd.to_s.camelcase}Command".constantize
-        cmd.new(args).run @context
+        cmd = "#{cmd.to_s.camelcase}Command".constantize.new args
       else
-        cmd = "#{cmd.to_s.camelcase}Command".constantize
-        cmd.new.run @context
+        cmd = "#{cmd.to_s.camelcase}Command".constantize.new
       end
+
+      cmd.run @context
     end
   end
 end
