16e66f7c2d2787a9039691c891db824b8ff235d2
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-16 11:47:40 -0300

    Added top tabs and more styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def verbo_version
+    begin
+      @@verbo_version = File.read('VERSION').strip unless defined? @@verbo_version
+    rescue Errno::ENOENT
+      @@verbo_version = 'Development'
+    end
+    @@verbo_version
+  end
 end
