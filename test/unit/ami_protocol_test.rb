634c512820fb7287312f6bc17fb7a931f21448fb
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-18 12:39:25 -0300

    Implemented ami protocol

--- test/unit/ami_protocol_test.rb
+++ test/unit/ami_protocol_test.rb
@@ -2,14 +2,38 @@ require 'test_helper'
 
 class AmiProtocolTest < ActiveSupport::TestCase
   test "send" do
-    seq = sequence('seq')
-
     ami = AmiProtocol.new nil
+
+    seq = sequence('seq')
     ami.expects(:send_data).with("action: login\n").in_sequence(seq)
     ami.expects(:send_data).with("username: foo\n").in_sequence(seq)
     ami.expects(:send_data).with("secret: bar\n").in_sequence(seq)
     ami.expects(:send_data).with("\n").in_sequence(seq)
 
-    ami.login :username => 'foo', :secret => 'bar'
+    Fiber.new { ami.login :username => 'foo', :secret => 'bar' }.resume
+  end
+
+  test "receive response" do
+    ami = AmiProtocol.new nil
+
+    ami.expects(:resume_fiber_with).with({:response => 'Success', :actionid => 'sarasa', :message => 'something'})
+
+    ami.receive_line("Asterisk Call Manager/1.1\n")
+    ami.receive_line("Response: Success\n")
+    ami.receive_line("ActionID: sarasa\n")
+    ami.receive_line("Message: something\n")
+    ami.receive_line("\n")
+  end
+
+  test "receive event" do
+    ami = AmiProtocol.new nil
+
+    ami.expects(:receive_event).with({:event => 'Hangup', :actionid => 'sarasa', :message => 'something'})
+
+    ami.receive_line("Asterisk Call Manager/1.1\n")
+    ami.receive_line("Event: Hangup\n")
+    ami.receive_line("ActionID: sarasa\n")
+    ami.receive_line("Message: something\n")
+    ami.receive_line("\n")
   end
 end
