6bf0d7915d1650a4cd344b80647644f840b723ec
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-22 17:06:18 -0300

    Now fixed the sample

--- samples/guess_the_number/main.rb
+++ samples/guess_the_number/main.rb
@@ -8,16 +8,24 @@ post '/' do
   number = numbers[session_id]
   pressed = params[:Digits].to_i
 
-  if number && pressed > 0
-    if number == pressed
-      '<Response><Play>http://localhost:4567/woman.gsm</Play><Hangup /></Response>'
-    elsif number > pressed
-      '<Response><Gather numDigits="2"><Play>http://localhost:4567/larger.mp3</Play></Gather></Response>'
+  if number
+    if pressed > 0
+      if number == pressed
+        '<Response><Play>http://localhost:4567/woman.gsm</Play><Hangup /></Response>'
+      elsif number > pressed
+        '<Response><Gather numDigits="2"><Play>http://localhost:4567/larger.mp3</Play></Gather></Response>'
+      else
+        '<Response><Gather numDigits="2"><Play>http://localhost:4567/smaller.mp3</Play></Gather></Response>'
+      end
     else
-      '<Response><Gather numDigits="2"><Play>http://localhost:4567/smaller.mp3</Play></Gather></Response>'
+      guess
     end
   else
     numbers[session_id] = (1..99).to_a.sample
-    '<Response><Gather numDigits="2"><Play>http://localhost:4567/guess.mp3</Play></Gather></Response>'
+    guess
   end
+end
+
+def guess
+  '<Response><Gather numDigits="2"><Play>http://localhost:4567/guess.mp3</Play></Gather></Response>'
 end
