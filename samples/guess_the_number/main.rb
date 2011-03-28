32a96831f97c3462100d6a4eda269a4bad0a5dd0
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-28 13:02:42 -0300

    Refactored guess the number sample for clarity

--- samples/guess_the_number/main.rb
+++ samples/guess_the_number/main.rb
@@ -8,24 +8,30 @@ post '/' do
   number = numbers[session_id]
   pressed = params[:Digits].to_i
 
-  if number
-    if pressed > 0
-      if number == pressed
-        '<Response><Play>http://localhost:4567/woman.gsm</Play><Hangup /></Response>'
-      elsif number > pressed
-        '<Response><Gather numDigits="2"><Play>http://localhost:4567/larger.mp3</Play></Gather></Response>'
-      else
-        '<Response><Gather numDigits="2"><Play>http://localhost:4567/smaller.mp3</Play></Gather></Response>'
-      end
-    else
-      guess
-    end
-  else
+  if not number
     numbers[session_id] = (1..99).to_a.sample
-    guess
+    return guess
+  end
+
+  return guess if pressed <= 0
+
+  if number == pressed
+    play 'woman.gsm'
+  elsif number > pressed
+    gather_with_file 'larger.mp3'
+  else
+    gather_with_file 'smaller.mp3'
   end
 end
 
 def guess
-  '<Response><Gather numDigits="2"><Play>http://localhost:4567/guess.mp3</Play></Gather></Response>'
+  gather_with_file 'guess.mp3'
+end
+
+def gather_with_file(file)
+  %Q(<Response><Gather numDigits="2"><Play>http://localhost:4567/#{file}</Play></Gather></Response>)
+end
+
+def play(file)
+  "<Response><Play>http://localhost:4567/#{file}</Play><Hangup /></Response>"
 end
