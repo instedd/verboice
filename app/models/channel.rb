ccc2aa92e8d1153da4d82b916741c779f9fbca01
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-03-31 14:49:56 -0300

    Use after_commit callbacks only on create and update for pbx config

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -5,16 +5,18 @@ class Channel < ActiveRecord::Base
   validates_presence_of :account
   validates_presence_of :application
 
-  after_commit :call_pbx_update_channel
+  after_commit :call_pbx_create_channel, :on => :create
+  after_commit :call_pbx_update_channel, :on => :update
   before_destroy :call_pbx_delete_channel
 
   serialize :config, Hash
 
   private
 
-  def call_pbx_update_channel
+  def call_pbx_create_channel
     PbxClient.update_channel self.id
   end
+  alias_method :call_pbx_update_channel, :call_pbx_create_channel
 
   def call_pbx_delete_channel
     PbxClient.delete_channel self.id
