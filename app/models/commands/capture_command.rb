e4002bdb0afe08823e933c32736c64acee84ae24
Ary Borenszweig <aborenszweig@manas.com.ar>
2011-09-14 15:54:56 -0300

    Shortened ui length for play and say in capture

--- app/models/commands/capture_command.rb
+++ app/models/commands/capture_command.rb
@@ -3,8 +3,8 @@ class CaptureCommand < Command
   param :max, :integer, :default => 1, :ui_length => 1
   param :finish_on_key, :string, :default => '#', :ui_length => 1
   param :timeout, :integer, :default => 5, :ui_length => 1
-  param :play, :string, :ui_length => 80
-  param :say, :string, :ui_length => 80
+  param :play, :string, :ui_length => 40
+  param :say, :string, :ui_length => 40
 
   def initialize(options = {})
     @options = {:min => 1, :max => 1, :finish_on_key => '#', :timeout => 5}
