03fcdfab836d08850ec1f719b5eb2d33007a5844
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-02-10 15:15:34 -0300

    Added retries field for call queue with validation

--- app/models/call_queue.rb
+++ app/models/call_queue.rb
@@ -1,7 +1,9 @@
 class CallQueue < ActiveRecord::Base
   belongs_to :account
+  has_many :queued_calls
 
   validates_presence_of :account
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false, :scoped_to => :account_id
+  validates_format_of :retries, :with => /^[0-9\.]+(,[0-9\.]+)*$/, :allow_blank => true
 end
--- spec/models/call_queue_spec.rb
+++ spec/models/call_queue_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe CallQueue do
   context "validations" do
-    before(:each) { CallQueue.make }
+    subject { CallQueue.make }
 
     it { should belong_to(:account) }
     it { should validate_presence_of(:account) }
     it { should validate_presence_of(:name) }
+    it { should_not allow_value("ABC").for(:retries) }
+    it { should allow_value("5").for(:retries) }
+    it { should allow_value("1,2,3").for(:retries) }
+    it { should allow_value("1.5").for(:retries) }
+    it { should_not allow_value("1,,2").for(:retries) }
+
+
   end
 end
