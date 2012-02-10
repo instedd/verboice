bd2336513656f91c5fc6ff783743ef312d8ead1a
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-02-10 15:43:45 -0300

    Added time from/to fields for call queue

--- app/models/call_queue.rb
+++ app/models/call_queue.rb
@@ -6,4 +6,26 @@ class CallQueue < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false, :scoped_to => :account_id
   validates_format_of :retries, :with => /^[0-9\.]+(,[0-9\.]+)*$/, :allow_blank => true
+
+  def time_from_str
+    time_str(time_from)
+  end
+
+  def time_from_str=(value)
+    self.time_from = value
+  end
+
+  def time_to_str
+    time_str(time_to)
+  end
+
+  def time_to_str=(value)
+    self.time_to = value
+  end
+
+  private
+
+  def time_str(time)
+    "#{time.hour}:#{'%02d' % time.min}"
+  end
 end
