75c0b9d5a3a82a905815ca0dc32783e0b4708095
nekron <none@none>
2012-04-04 16:59:53 -0300

    TraceCommand created
    --HG--
    branch : workflow-designer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120403143009) do
+ActiveRecord::Schema.define(:version => 20120404194246) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -93,5 +93,16 @@ ActiveRecord::Schema.define(:version => 20120403143009) do
     t.datetime "not_before"
     t.integer  "retries",             :default => 0
   end
+
+  create_table "traces", :force => true do |t|
+    t.integer  "application_id"
+    t.integer  "step_id"
+    t.integer  "call_id"
+    t.string   "result"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "traces", ["application_id"], :name => "index_traces_on_application_id"
 
 end
