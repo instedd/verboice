a238d07127dcecfdf85c198f202836474ba7e4df
Adrian Romero <aromero@manas.com.ar>
2012-04-12 15:28:19 -0300

    Added commands to voice xml builder

--- app/models/builders/vxml.rb
+++ app/models/builders/vxml.rb
@@ -9,17 +9,79 @@ module Builders
       xml = @xml_builder.to_xml
       init_xml_builder
       xml
-      # <<-xml
-      #   <?xml version="1.0" encoding="UTF-8"?><vxml version="2.1"><form><field name="capture" type="digits?length=2"><grammar type="text/gsl">[dtmf-1 dtmf-2 dtmf-3 dtmf-4 dtmf-5 dtmf-6 dtmf-7 dtmf-8 dtmf-9 dtmf-0]</grammar><prompt><break strength="2s"/>Guess the number, it's between 1 and 99</prompt><noinput><prompt>Try Again</prompt><reprompt/></noinput><nomatch><prompt>You pressed a wrong input. Try Again.</prompt><reprompt/></nomatch></field><block><submit next="http://staging.instedd.org:7000/" method="GET"/></block></form></vxml>
-      # xml
     end
     
-    def say text
+    def say(text)
       append_to_form do |xml|
         xml.block do
           xml.prompt text
         end
       end
+      
+      self
+    end
+    
+    def play(filename)
+      append_to_form do |xml|
+        xml.block do
+          xml.audio(:src => filename)
+        end
+      end
+      
+      self
+    end
+    
+    def pause(time)
+      append_to_form do |xml|
+        xml.block do
+          xml.prompt do
+            xml.break(:time => "#{time}s")
+          end
+        end
+      end
+      
+      self
+    end
+    
+    def hangup
+      append_to_form do |xml|
+        xml.block do
+          xml.exit
+        end
+      end
+      
+      self
+    end
+    
+    def callback(url, method = :get)
+      append_to_form do |xml|
+        xml.block do
+          xml.submit_(:next => url, :method => method.to_s.upcase)
+        end
+      end
+      
+      self
+    end
+    
+    def capture(options)
+      min_digits = options[:min] || 1
+      max_digits = options[:max] || 1
+      timeout = options[:timeout] || 10
+      
+      append_to_form do |xml|
+        xml.field(:name => "digits", :type => "digits?minlength=#{min_digits};maxlength=#{max_digits}") do
+          xml.grammar "[dtmf-1 dtmf-2 dtmf-3 dtmf-4 dtmf-5 dtmf-6 dtmf-7 dtmf-8 dtmf-9 dtmf-0]", :type => "text/gsl"
+          xml.property(:name => "timeout", :value => "#{timeout}s")
+          xml.noinput
+          xml.nomatch
+          if options[:say]
+            xml.prompt options[:say]
+          elsif options[:play]
+            xml.audio :src => options[:play]
+          end
+        end
+      end
+      
       self
     end
     
@@ -36,25 +98,38 @@ module Builders
       @xml_builder = Nokogiri::XML::Builder.new do |xml|
         xml.vxml(:version => "2.1") do
           # xml.catch_(:event => "connection.disconnect.hangup") do
+          #   xml.log(:expr => "'*** CATCH EVENT DISCONNECTED ***'")
+          #   xml.submit_(:next => "http://staging.instedd.org:7000/?disconnect=true")
+          #   xml.exit
+          # end
+          # xml.error do
+          #   xml.log(:expr => "'*** CATCH ERROR ***'")
           #   xml.submit_(:next => "http://staging.instedd.org:7000/?disconnect=true")
           #   xml.exit
           # end
           xml.form do
             @form = xml.parent
+            # xml.block do
+            #   xml.throw_(:event => "error.foo")
+            # end
+            # xml.block do
+            #   xml.prompt "Before disconnect"
+            # end
+            # xml.block do
+            #   xml.disconnect
+            # end
+            # xml.field do
+            #   # xml.property(:name => "timeout", :value => "1s")
+            #   xml.grammar "[(kathy bates is really hot)]"
+            #   xml.nomatch do
+            #     xml.exit
+            #   end
+            #   xml.noinput do
+            #     xml.exit
+            #   end
+            #   # xml.exit
+            # end
           end
-          # dummy form
-          # xml.form do
-          #             xml.field(:name => "_dummyField") do
-          #               xml.prompt
-          #               xml.grammar "[(dummy grammar)]"
-          #               xml.filled do
-          #                 xml.prompt
-          #               end
-          #               xml.noinput do
-          #                 xml.disconnect
-          #               end
-          #             end
-          #           end
         end
       end
     end
--- spec/models/builders/vxml_spec.rb
+++ spec/models/builders/vxml_spec.rb
@@ -28,4 +28,141 @@ describe Builders::Vxml do
     vxml.say("Hello World").build.should be_equivalent_to(xml)
   end
   
+  it "build a prompt block for pause" do
+    xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <vxml version="2.1">
+        <form>
+          <block>
+            <prompt>
+              <break time="5s"/>
+            </prompt>
+          </block>
+        </form>
+      </vxml>
+    XML
+    
+    vxml.pause(5).build.should be_equivalent_to(xml)
+  end
+  
+  it "should hangup with exit within block" do
+    xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <vxml version="2.1">
+        <form>
+          <block>
+            <exit/>
+          </block>
+        </form>
+      </vxml>
+    XML
+    
+    vxml.hangup.build.should be_equivalent_to(xml)
+  end
+  
+  it "should play using audio within block" do
+    xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <vxml version="2.1">
+        <form>
+          <block>
+            <audio src="foo.wav"/>
+          </block>
+        </form>
+      </vxml>
+    XML
+    
+    vxml.play("foo.wav").build.should be_equivalent_to(xml)
+  end
+  
+  it "builds a callback using submit" do
+    xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <vxml version="2.1">
+        <form>
+          <block>
+            <submit next="http://www.foo.com/bar" method="GET"/>
+          </block>
+        </form>
+      </vxml>
+    XML
+    
+    vxml.callback("http://www.foo.com/bar").build.should be_equivalent_to(xml)
+  end
+  
+  it "builds a POST callback using submit" do
+    xml = <<-XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <vxml version="2.1">
+        <form>
+          <block>
+            <submit next="http://www.foo.com/bar" method="POST"/>
+          </block>
+        </form>
+      </vxml>
+    XML
+    
+    vxml.callback("http://www.foo.com/bar", :post).build.should be_equivalent_to(xml)
+  end
+  
+  context "capture" do
+  
+    it "should capture with say using field" do
+      xml = <<-XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <vxml version="2.1">
+          <form>
+            <field name="digits" type="digits?minlength=1;maxlength=1">
+              <grammar type="text/gsl">[dtmf-1 dtmf-2 dtmf-3 dtmf-4 dtmf-5 dtmf-6 dtmf-7 dtmf-8 dtmf-9 dtmf-0]</grammar>
+              <property name="timeout" value="10s"/>
+              <noinput/>
+              <nomatch/>
+              <prompt>This is a capture</prompt>
+            </field>
+          </form>
+        </vxml>
+      XML
+    
+      vxml.capture({:say => "This is a capture"}).build.should be_equivalent_to(xml)
+    end
+    
+    it "should capture with play using field" do
+      xml = <<-XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <vxml version="2.1">
+          <form>
+            <field name="digits" type="digits?minlength=1;maxlength=1">
+              <grammar type="text/gsl">[dtmf-1 dtmf-2 dtmf-3 dtmf-4 dtmf-5 dtmf-6 dtmf-7 dtmf-8 dtmf-9 dtmf-0]</grammar>
+              <property name="timeout" value="10s"/>
+              <noinput/>
+              <nomatch/>
+              <audio src="capture.wav"/>
+            </field>
+          </form>
+        </vxml>
+      XML
+    
+      vxml.capture({:play => "capture.wav"}).build.should be_equivalent_to(xml)
+    end
+    
+    it "should capture with options using field" do
+      xml = <<-XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <vxml version="2.1">
+          <form>
+            <field name="digits" type="digits?minlength=5;maxlength=10">
+              <grammar type="text/gsl">[dtmf-1 dtmf-2 dtmf-3 dtmf-4 dtmf-5 dtmf-6 dtmf-7 dtmf-8 dtmf-9 dtmf-0]</grammar>
+              <property name="timeout" value="20s"/>
+              <noinput/>
+              <nomatch/>
+              <prompt>This is a capture</prompt
+            </field>
+          </form>
+        </vxml>
+      XML
+    
+      vxml.capture(:say => "This is a capture", :min => 5, :max => 10, :timeout => 20).build.should be_equivalent_to(xml)
+    end
+  end
+  
 end
