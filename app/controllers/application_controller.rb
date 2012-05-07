43a34aa1eb31c0af2bbb2279bfaf3c3d6dd9037e
spalladino <none@none>
2012-05-07 12:45:39 -0300

    Set fixed width for home page
    --HG--
    extra : rebase_source : a0fb5a41c01810dbab45aeb7522424a8bb480c24

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   before_filter do
     @body_class = ['full-width']
   end
+
+  def set_fixed_width_content
+    @body_class << 'fixed-width-content'
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    set_fixed_width_content
   end
 end
