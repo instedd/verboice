efd987ccb4f0ab60650c895317572b0f93466186
Adrian Romero <aromero@manas.com.ar>
2012-05-16 16:05:54 -0300

    Fixed bug related to empty query string values in http context

--- app/models/voxeo/http_context.rb
+++ app/models/voxeo/http_context.rb
@@ -1,29 +1,31 @@
+require 'cgi'
+
 module Voxeo
   class HttpContext
-  
+
     def initialize http_headers, http_query_string
       @http_headers = http_headers
       @http_query_string = http_query_string
     end
-  
+
     def headers
       @headers ||= init_headers
     end
-  
+
     def params
       @params ||= init_params
     end
-  
+
     private
-  
+
     def init_params
       if @http_query_string
-        @http_query_string.split('&').map{|x|x.split('=')}.inject(HashWithIndifferentAccess.new){|r,x|r[x.first]=CGI.unescape(x.second);r}
+        @http_query_string.split('&').map{|x|x.split('=')}.inject(HashWithIndifferentAccess.new){|r,x|r[x.first]=CGI.unescape(x.second || '');r}
       else
         HashWithIndifferentAccess.new
       end
     end
-  
+
     def init_headers
       if @http_headers
         @http_headers.split("\x00").map{|x|x.split(':', 2)}.inject(HashWithIndifferentAccess.new){|r,x|r[x.first.strip]=x.second.strip;r}
@@ -31,6 +33,6 @@ module Voxeo
         HashWithIndifferentAccess.new
       end
     end
-    
+
   end
 end
--- spec/models/voxeo/http_context_spec.rb
+++ spec/models/voxeo/http_context_spec.rb
@@ -1,33 +1,39 @@
 require 'spec_helper'
 
 describe Voxeo::HttpContext do
-  
+
   let :context do
     headers = "Header1: Header1Value\x00Header2: HeaderWith:Colon"
     query_string = "key1=value1&key2=value2"
     Voxeo::HttpContext.new headers, query_string
   end
-  
+
   it "should tell headers" do
     context.headers[:Header1].should eq("Header1Value")
     context.headers[:Header2].should eq("HeaderWith:Colon")
   end
-  
+
   it "should tell params" do
     context.params[:key1].should eq('value1')
     context.params[:key2].should eq('value2')
   end
-  
+
   it "should have empty headers and params for nil headers and qs" do
     context = Voxeo::HttpContext.new nil, nil
     context.headers.empty?.should be_true
     context.params.empty?.should be_true
   end
-  
+
   it "should unescape query string" do
     value = "a value that & needs to be escaped"
     context = Voxeo::HttpContext.new nil, "key=#{CGI.escape(value)}"
     context.params[:key].should eq(value)
   end
-  
+
+  it "should support empty query string values" do
+    qs = "key="
+    context = Voxeo::HttpContext.new nil, qs
+    context.params[:key].should eq('')
+  end
+
 end
