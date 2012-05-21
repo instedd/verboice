87e054adbb8ff079e8c3ad0f8293b35c98514f29
Adrian Romero <aromero@manas.com.ar>
2012-05-21 17:03:04 -0300

    Added some tests to record_audio_controller_spec

--- spec/controllers/recorded_audios_controller_spec.rb
+++ spec/controllers/recorded_audios_controller_spec.rb
@@ -8,8 +8,10 @@ describe RecordedAudiosController do
   end
   let!(:account) { Account.make }
   let!(:contact) { Contact.make account: account }
+  let!(:other_contact) { Contact.make }
   let!(:call_log) { CallLog.make account: account }
   let!(:recorded_audio) { RecordedAudio.make contact: contact }
+  let!(:other_recorded_audio) { RecordedAudio.make contact: other_contact }
 
   describe "GET index" do
     it "assigns all recorded_audios as @recorded_audios" do
@@ -23,6 +25,13 @@ describe RecordedAudiosController do
       get :show, {:id => recorded_audio.to_param, :contact_id => contact.to_param}
       assigns(:recorded_audio).should eq(recorded_audio)
     end
+
+    it "fails if the requested recorded_audio is not in current account contacts" do
+      expect {
+        get :show, {:id => other_recorded_audio.to_param, :contact_id => other_contact.to_param}
+      }.should raise_error
+      assigns(:recorded_audio).should be_nil
+    end
   end
 
   describe "DELETE destroy" do
@@ -35,6 +44,14 @@ describe RecordedAudiosController do
     it "redirects to the recorded_audios list" do
       delete :destroy, {:id => recorded_audio.to_param, :contact_id => contact.to_param}
       response.should redirect_to(contact_recorded_audios_path(contact))
+    end
+
+    it "fails if the requested recorded_audio is not in current account contacts" do
+      expect {
+        delete :destroy, {:id => other_recorded_audio.to_param, :contact_id => other_contact.to_param}
+      }.should raise_error
+      assigns(:recorded_audio).should be_nil
+      RecordedAudio.find(other_recorded_audio.id).should eq(other_recorded_audio)
     end
   end
 
