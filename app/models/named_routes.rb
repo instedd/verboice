fa3de1461eddd747704be6dfe27a781c665b4a60
Adrian Romero <aromero@manas.com.ar>
2012-05-21 13:49:44 -0300

    Moved default url options config from /config/application.rb to verboice.yml

--- app/models/named_routes.rb
+++ app/models/named_routes.rb
@@ -2,8 +2,10 @@ class NamedRoutes
   include Singleton
   include Rails.application.routes.url_helpers
 
+  DefaultUrlOptions = Rails.configuration.verboice_configuration[:default_url_options].to_hash.symbolize_keys!
+
   def self.default_url_options
-    Rails.application.config.default_url_options
+    DefaultUrlOptions
   end
 
   def self.method_missing(method_sym, *arguments, &block)
