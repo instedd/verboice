707cdb33d45f909220969f29d139209acc48444b
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-05-22 17:57:26 -0300

    Workaround to unmarshal large flows inside a fiber

--- app/models/marshal_zip_serializable.rb
+++ app/models/marshal_zip_serializable.rb
@@ -8,7 +8,12 @@ module MarshalZipSerializable
 
     def load(x)
       return nil if x.nil?
-      Marshal.load(Zlib.inflate(x)) rescue nil
+      data = nil
+
+      # Marshal.load fails with large flows when run inside a Fiber (Fiber stacks are 4k only)
+      # Run the unmarshaling in a thread so we can use a full stack
+      Thread.new { data = Marshal.load(Zlib.inflate(x)) rescue nil }.join
+      data
     end
   end
 
