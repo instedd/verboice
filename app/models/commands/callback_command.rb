a834a4eada801c1055fa994255de6faf46284702
Adrian Romero <aromero@manas.com.ar>
2012-05-31 16:11:24 -0300

    Backout changeset e3beb1441116470d3ff5536e94b2e57f9ebb30b4
    --HG--
    branch : external_services

--- app/models/commands/callback_command.rb
+++ app/models/commands/callback_command.rb
@@ -1,6 +1,4 @@
 class Commands::CallbackCommand < Command
-  include Commands::Callback
-
   attr_accessor :url
   attr_accessor :method
   attr_accessor :params
@@ -13,38 +11,47 @@ class Commands::CallbackCommand < Command
 
   def run(session)
     url = @url || session.callback_url
-    url, authentication = callback_authentication(url, session.call_flow)
-    method = @method.to_s.downcase.to_sym
+    method = (@method || 'post').to_s.downcase.to_sym
 
-    callback_options = {}
-    callback_options[:authentication] = authentication
-    callback_options[:method] = method
-    callback_options[:params] = {:CallSid => session.call_id, :From => session.pbx.caller_id, :Channel => session.channel.name}
+    url, authorization = callback_authentication(url, session.call_flow)
+
+    body = {:CallSid => session.call_id, :From => session.pbx.caller_id, :Channel => session.channel.name}
     if @params
       @params.each do |name, key|
-        callback_options[:params][name] = session[key]
+        body[name] = session[key]
       end
     end
+    session.log :info => "Callback #{method} #{url}", :trace => "Callback #{method} #{url} with #{body.to_query}"
 
-    session.log :info => "Callback #{method} #{url}", :trace => "Callback #{method} #{url} with #{callback_options[:params].to_query}"
+    http = http_request(url, method, body, authorization)
 
-    http = callback(url, callback_options)
+    f = Fiber.current
+    http.callback do
+      begin
+        if http.response_header.status.to_i != 200
+          raise "Callback failed with status #{http.response_header.status}"
+        end
 
-    content_type = http.response_header[EventMachine::HttpClient::CONTENT_TYPE]
-    body = http.response
+        content_type = http.response_header[EventMachine::HttpClient::CONTENT_TYPE]
+        body = http.response
 
-    session.trace "Callback returned #{content_type}: #{body}"
+        session.trace "Callback returned #{content_type}: #{body}"
 
-    next_command =  case content_type
-                    when %r(application/json)
-                      Commands::JsCommand.new body
-                    else
-                      Parsers::Xml.parse body
-                    end
+        commands = case content_type
+                   when %r(application/json)
+                     commands = Commands::JsCommand.new body
+                   else
+                     commands = Parsers::Xml.parse body
+                   end
 
-    next_command.last.next = self.next
-
-    next_command
+        commands.last.next = self.next
+        f.resume commands
+      rescue Exception => e
+        f.resume e
+      end
+    end
+    http.errback { f.resume Exception.new(http.error.present? ? http.error : "Failed to communicate with #{url}") }
+    Fiber.yield
   end
 
   private
@@ -57,9 +64,19 @@ class Commands::CallbackCommand < Command
     callback_url_password = uri.password || project.callback_url_password
     uri.password = nil
 
-    authentication = {:user => callback_url_user, :password => callback_url_password} if callback_url_user.present? || callback_url_password.present?
+    authorization = {:head => {'authorization' => [callback_url_user, callback_url_password]}} if callback_url_user.present? || callback_url_password.present?
 
-    [uri.to_s, authentication]
+    [uri.to_s, authorization]
   end
 
+  def http_request(url, method, body, authorization = {})
+    authorization ||= {}
+    request = EventMachine::HttpRequest.new(url)
+
+    if method == :get
+      request.get(body.merge(authorization))
+    else
+      request.post({:body => body}.merge(authorization))
+    end
+  end
 end
