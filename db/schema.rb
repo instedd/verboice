98c5113428533affa7afaa183412996e606cdd63
nekron <none@none>
2012-06-01 16:40:04 -0300

    Fixed #183: Call Flow ids are updated from project ids for channels, traces, queued_calls, and call_logs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120601171031) do
+ActiveRecord::Schema.define(:version => 20120601191115) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
