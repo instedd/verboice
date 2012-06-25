f87608c66c2efa99ebf552436fba9c645c5f9fa0
spalladino <none@none>
2012-06-25 13:17:24 -0300

    Converted user flow branch step condition from string to integer
    --HG--
    extra : rebase_source : 9237d7f764352a462558c4a7a0788187621aa764

---
+++ db/migrate/20120625155916_fix_user_flow_branch_step_i_ds.rb
@@ -0,0 +1,29 @@
+class FixUserFlowBranchStepIDs < ActiveRecord::Migration
+  def up
+    connection.select_rows("SELECT id, user_flow FROM call_flows WHERE user_flow IS NOT NULL AND user_flow <> ''").each do |call_flow_id, user_flow|
+      begin
+        data = Marshal.load(Zlib.inflate(user_flow))
+        next if data.nil?
+        puts "Processing call flow #{call_flow_id}"
+        data.select{|step| step['type'] == 'branch'}.each do |step|
+          step['options'].each do |option|
+            option['conditions'].each do |condition|
+              step = condition['step']
+              next if step.blank?
+              puts " Fixing condition for step #{step['name']} referring to step #{step}"
+              condition['step'] = step.to_i
+            end if option['conditions']
+          end if step['options']
+        end
+        binary_user_flow = Zlib.deflate(Marshal.dump(data), 9)
+        connection.execute("UPDATE call_flows SET user_flow=x'#{binary_user_flow.unpack("H*")[0]}' WHERE call_flows.id = #{call_flow_id}")
+      rescue Exception => ex
+        puts "Exception fixing call flow #{call_flow_id}: #{ex.message}\n#{ex.backtrace.join("\n")}"
+        raise
+      end
+    end
+  end
+
+  def down
+  end
+end
