872f82f3d7ce1c39c126a8f3515c660abc4f3f0b
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-06-26 16:33:43 -0300

    Run flow marshaling in a separate thread to avoid fiber stack limits

--- app/models/marshal_zip_serializable.rb
+++ app/models/marshal_zip_serializable.rb
@@ -1,17 +1,17 @@
 # Copyright (C) 2010-2012, InSTEDD
-# 
+#
 # This file is part of Verboice.
-# 
+#
 # Verboice is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Verboice is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -20,7 +20,12 @@ module MarshalZipSerializable
 
   module ClassMethods
     def dump(x)
-      Zlib.deflate(Marshal.dump(x), 9)
+      data = nil
+
+      # Marshal.dump fails with large flows when run inside a Fiber (Fiber stacks are 4k only)
+      # Run the marshaling in a thread so we can use a full stack
+      Thread.new { data = Zlib.deflate(Marshal.dump(x), 9) }.join
+      data
     end
 
     def load(x)
