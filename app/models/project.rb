1f21c6daa62a0c225d56e77144951247e256d711
nekron <none@none>
2012-07-19 18:39:42 -0300

    Fixed #306: Log foreign key is nullified when it's project is deleted.

--- app/models/project.rb
+++ app/models/project.rb
@@ -23,7 +23,7 @@ class Project < ActiveRecord::Base
   belongs_to :default_call_flow, :class_name => "CallFlow", :foreign_key => "call_flow_id"
 
   has_many :call_flows, :dependent => :destroy
-  has_many :call_logs, :dependent => :destroy
+  has_many :call_logs, :dependent => :nullify
   has_many :queued_calls, :dependent => :destroy
   has_many :external_services, :dependent => :destroy
   has_many :external_service_steps, :through => :external_services
