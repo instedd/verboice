8fd7732271d5e6f87b9b57b9fe86e4f52effb797
nekron <none@none>
2012-08-03 16:28:44 -0300

    Resource GUID migration fixed
    --HG--
    branch : resources

--- db/migrate/20120803143125_add_guid_to_existing_resources.rb
+++ db/migrate/20120803143125_add_guid_to_existing_resources.rb
@@ -1,10 +1,10 @@
 class AddGuidToExistingResources < ActiveRecord::Migration
   def up
     Resource.find_each do |resource|
-      connection.execute "UPDATE resources SET guid = #{Guid.new.to_s} WHERE id = #{resource.id}"
+      connection.execute "UPDATE resources SET guid = '#{Guid.new.to_s}' WHERE id = #{resource.id}"
     end
     LocalizedResource.find_each do |localized_resource|
-      connection.execute "UPDATE localized_resources SET guid = #{Guid.new.to_s} WHERE id = #{localized_resource.id}"
+      connection.execute "UPDATE localized_resources SET guid = '#{Guid.new.to_s}' WHERE id = #{localized_resource.id}"
     end
   end
   def down
