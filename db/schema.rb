72209596c5b0ffa05645ba85d2fd53e01c34a6df
Adrian Romero <aromero@manas.com.ar>
2012-08-06 10:49:43 -0300

    Fixed #319: migration to update user flow to use the resources model
    --HG--
    branch : resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120803143533) do
+ActiveRecord::Schema.define(:version => 20120803145851) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
