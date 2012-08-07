3c643dc41b4ee7af277f65924806614fdbed8095
Adrian Romero <aromero@manas.com.ar>
2012-08-07 11:14:25 -0300

    Fixed #305: Added language autocomplete to contact form

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -2,4 +2,8 @@ $(function() {
   $('.add_project_variable .ux-optext').live('click', function(){
     $(this).next(".clist-add").click();
   });
+
+  $('.edit_variable.language').autocomplete({
+    source: window.languages
+  });
 });
