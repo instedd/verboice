f1fa39ccdb11a0306ee58c6210f6a53d2fcacc97
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-11-21 18:14:33 -0300

    Don't check dependencies in test mode

--- config/initializers/check_dependencies.rb
+++ config/initializers/check_dependencies.rb
@@ -1,6 +1,8 @@
-%w(sox lame).each do |program|
-  unless `which #{program}` && $?.success?
-    puts "Error: #{program} not found. Please install #{program}."
-    exit(1)
+unless Rails.env == 'test'
+  %w(sox lame).each do |program|
+    unless `which #{program}` && $?.success?
+      puts "Error: #{program} not found. Please install #{program}."
+      exit(1)
+    end
   end
 end
