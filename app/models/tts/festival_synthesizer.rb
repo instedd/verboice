0260d424c04c3aecd87cd7c5a5d7d6fefd1be5f9
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-11-22 17:51:11 -0300

    List Festival voices
    --HG--
    extra : rebase_source : 7341d55c4130c9b4d03507b0f91bd263a84ff86a

--- app/models/tts/festival_synthesizer.rb
+++ app/models/tts/festival_synthesizer.rb
@@ -6,4 +6,24 @@ class TTS::FestivalSynthesizer < TTS::SystemSynthesizer
   def is_available?
     `which text2wave` && $?.success?
   end
+
+  def voices
+    @voices ||= load_voices
+  end
+
+  private
+
+  def load_voices
+    voices = Hash.new { |h, k| h[k] = [] }
+    IO.popen 'festival --pipe', 'r+' do |festival|
+      festival.write File.read(File.expand_path('../list_festival_voices.scm', __FILE__))
+      festival.close_write
+      festival.readlines.each do |line|
+        voice, language, gender, dialect = line.chomp!.split('|')
+        lang_code = LanguageList::LanguageInfo.find_by_name(language.titleize).iso_639_1
+        voices[lang_code] << {id: voice, description: "#{dialect.titleize} #{language.titleize} #{gender.titleize} (#{voice})"}
+      end
+    end
+    voices
+  end
 end
---
+++ app/models/tts/list_festival_voices.scm
@@ -0,0 +1,23 @@
+(define (print_voice voice)
+  (let (
+           (name (car voice))
+           (desc (cadr voice))
+         )
+         (let (
+                (language (assoc 'language desc))
+                (gender (assoc 'gender desc))
+                (dialect (assoc 'dialect desc))
+              )
+              (format t "%s|%s|%s|%s\n" name (cadr language) (cadr gender) (cadr dialect))
+         )
+    )
+)
+
+(define (print_voices voices)
+  (while voices
+    (print_voice (voice.description (car voices)))
+    (set! voices (cdr voices))
+  )
+)
+
+(print_voices (voice.list))
