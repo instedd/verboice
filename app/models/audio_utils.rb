253ff86c81f3e4847b6a7dd2afafb17f32da1c78
Juan Wajnerman <jwajnerman@manas.com.ar>
2012-11-28 18:47:56 -0300

    Added missing license headers

--- app/models/audio_utils.rb
+++ app/models/audio_utils.rb
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Verboioce.  If not, see <http://www.gnu.org/licenses/>.
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'tempfile'
 
--- app/models/channels/custom.rb
+++ app/models/channels/custom.rb
@@ -1,3 +1,20 @@
+# Copyright (C) 2010-2012, InSTEDD
+#
+# This file is part of Verboice.
+#
+# Verboice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Verboice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
+
 class Channels::Custom < Channel
 
   validates_numericality_of :limit, :only_integer => true, :greater_than => 0, :if => :has_limit?
