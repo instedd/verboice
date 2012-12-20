df72eb974533396c561b10ad3a6132525a9be7fc
Ary Borenszweig <aborenszweig@manas.com.ar>
2012-12-20 10:49:14 -0300

    Expose projects and its flows in API

---
+++ app/controllers/api/projects_controller.rb
@@ -0,0 +1,35 @@
+# Copyright (C) 2010-2012, InSTEDD
+#
+# This file is part of Verboice.
+#
+# Verboice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Verboice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
+module Api
+  class ProjectsController < ApiController
+    def index
+      projects = current_account.projects.includes(:call_flows).map do |project|
+        {
+          id: project.id,
+          name: project.name,
+          call_flows: project.call_flows.map do |call_flow|
+            {
+              id: call_flow.id,
+              name: call_flow.name,
+            }
+          end,
+        }
+      end
+      render json: projects
+    end
+  end
+end
---
+++ spec/controllers/api/projects_controllers_spec.rb
@@ -0,0 +1,41 @@
+# Copyright (C) 2010-2012, InSTEDD
+#
+# This file is part of Verboice.
+#
+# Verboice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Verboice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'spec_helper'
+
+describe Api::ProjectsController do
+  include Devise::TestHelpers
+
+  before(:each) do
+    sign_in account
+  end
+  let!(:account) { Account.make }
+  let!(:project) { account.projects.make }
+  let!(:call_flow) { project.call_flows.make }
+
+  it "should list all projects" do
+    get :index
+
+    response = JSON.parse(@response.body)
+    response.length.should == 1
+    response[0]['id'].should eq(project.id)
+    response[0]['name'].should eq(project.name)
+    response[0]['call_flows'].length.should eq(1)
+    response[0]['call_flows'][0]['id'].should eq(call_flow.id)
+    response[0]['call_flows'][0]['name'].should eq(call_flow.name)
+  end
+end
