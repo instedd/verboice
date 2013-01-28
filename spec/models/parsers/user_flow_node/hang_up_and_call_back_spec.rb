e1f59a12d44dae6ec2da5e17338d55579bb0808f
Ary Borenszweig <aborenszweig@manas.com.ar>
2013-01-28 18:33:11 -0300

    AAdded spec to hang up and call back user flow node

---
+++ spec/models/parsers/user_flow_node/hang_up_and_call_back_spec.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2010-2012, InSTEDD
+#
+# This file is part of Verboice.
+#
+# Verboice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Verboice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'spec_helper'
+
+module Parsers
+  module UserFlowNode
+    describe HangUpAndCallBack do
+
+      let(:call_flow) { CallFlow.make }
+
+      it "should compile to a verboice equivalent flow" do
+        File.stub(:exists?).and_return{true}
+        mark_as_failed = HangUpAndCallBack.new call_flow, 'id' => 1,
+          'type' => 'hang_up_and_call_back',
+          'name' => 'HangUpAndCallBack'
+
+        mark_as_failed.equivalent_flow.first.should eq(
+          Compiler.parse do |c|
+            c.Label 1
+            c.Assign "current_step", 1
+            c.AssignValue "current_step_name", "HangUpAndCallBack"
+            c.Trace call_flow_id: call_flow.id, step_id: 1, step_name: 'HangUpAndCallBack', store: '"Hang up and call back."'
+            c.HangupAndCallback
+          end.first
+        )
+      end
+    end
+  end
+end
