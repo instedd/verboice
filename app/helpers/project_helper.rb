950e06f24ea92a1673042e2d3fdfcf010e877c9e
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-02-14 17:51:08 -0300

    Update language_list gem and use the entire list of languages with ISO639-1 code

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -18,7 +18,7 @@
 module ProjectHelper
 
   def languages_for_js
-    LanguageList::COMMON_LANGUAGES.map{|l| {label: l.name, value: l.iso_639_1}}.to_json
+    LanguageList::ISO_639_1.map{|l| {label: l.name, value: l.iso_639_1}}.to_json
   end
 
   def project_languages_json(project)
