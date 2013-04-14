9a3dfb2a0f8fe5c6a4232a79b558b10c1f46be0c
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-04-14 11:57:25 -0300

    Use erlang:nif_error in mozjs module so dialyzer can be used
    --HG--
    branch : new-broker

--- broker/src/mozjs.erl
+++ broker/src/mozjs.erl
@@ -7,14 +7,17 @@ init() ->
   File = filename:join(filename:dirname(Ebin), "priv/verboice_drv"),
   ok = erlang:load_nif(File, 0).
 
+-spec new_runtime() -> port().
 new_runtime() ->
-  exit(nif_library_not_loaded).
+  erlang:nif_error(module_not_loaded, []).
 
-new_context(_Runtime) ->
-  exit(nif_library_not_loaded).
+-spec new_context(port()) -> port().
+new_context(Runtime) ->
+  erlang:nif_error(module_not_loaded, [Runtime]).
 
 shutdown() ->
-  exit(nif_library_not_loaded).
+  erlang:nif_error(module_not_loaded, []).
 
-eval(_Context, _Script) ->
-  exit(nif_library_not_loaded).
+-spec eval(port(), string()) -> term().
+eval(Context, Script) ->
+  erlang:nif_error(module_not_loaded, [Context, Script]).
