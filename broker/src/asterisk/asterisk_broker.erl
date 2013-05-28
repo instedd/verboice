2d442380140b4d3fddb643c524a19eb3f24221b4
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-05-28 12:22:55 +0700

    Support dialing through custom channels
    --HG--
    branch : new-broker

--- broker/src/asterisk/asterisk_broker.erl
+++ broker/src/asterisk/asterisk_broker.erl
@@ -16,14 +16,25 @@ init() ->
 notify_ready() ->
   broker:notify_ready(?MODULE).
 
-dispatch(Session = #session{session_id = SessionId}) ->
-  ChannelId = integer_to_list(Session#session.channel#channel.id),
-  Address = "SIP/verboice_" ++ ChannelId ++ "-outbound/" ++ binary_to_list(Session#session.address),
+dial_address(#channel{type = <<"Channels::Custom">>, config = Config}, Address) ->
+  DialString = proplists:get_value(<<"dial_string">>, Config),
+  util:interpolate(DialString, fun(Key) ->
+    case Key of
+      <<"number">> -> Address;
+      _ -> <<>>
+    end
+  end);
+
+dial_address(#channel{id = Id}, Address) ->
+  ["SIP/verboice_", integer_to_list(Id), "-outbound/", Address].
+
+dispatch(#session{session_id = SessionId, channel = Channel, address = Address}) ->
+  DialAddress = dial_address(Channel, Address),
   {ok, BrokerPort} = application:get_env(broker_port),
   ami_client:originate([
-    {channel, Address},
+    {channel, DialAddress},
     {application, "AGI"},
-    {data, "agi://localhost:" ++ integer_to_list(BrokerPort) ++ "," ++ SessionId},
+    {data, ["agi://localhost:", integer_to_list(BrokerPort), ",", SessionId]},
     {async, true},
     {actionid, SessionId}
   ]).
