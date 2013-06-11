2de57c596913d4ee7a4a41186279962164826a8b
Gustavo Giráldez <ggiraldez@manas.com.ar>
2013-06-11 18:47:33 -0300

    Show SMS channels tab and Nuntium step if Nuntium is configured with Pigeon
    --HG--
    branch : nuntium-integration

--- config/initializers/pigeon.rb
+++ config/initializers/pigeon.rb
@@ -2,16 +2,16 @@ nuntium_config_path = "#{Rails.root}/config/nuntium.yml"
 nuntium_config = File.exists?(nuntium_config_path) ? YAML::load(File.open(nuntium_config_path))[Rails.env] : {}
 
 Pigeon.setup do |config|
-    config.application_name = 'Verboice'
+  config.application_name = 'Verboice'
 
-    config.nuntium_host = nuntium_config['host'] || 'http://nuntium.instedd.org'
-    config.nuntium_account = nuntium_config['account']
-    config.nuntium_app = nuntium_config['application']
-    config.nuntium_app_password = nuntium_config['password']
+  config.nuntium_host = nuntium_config['host']
+  config.nuntium_account = nuntium_config['account']
+  config.nuntium_app = nuntium_config['application']
+  config.nuntium_app_password = nuntium_config['password']
 
-    # If you want to support Nuntium Twitter channels, get your Twitter
-    # consumer keys from https://dev.twitter.com/apps
-    config.twitter_consumer_key = nuntium_config['twitter_consumer_key']
-    config.twitter_consumer_secret = nuntium_config['twitter_consumer_secret']
+  # If you want to support Nuntium Twitter channels, get your Twitter
+  # consumer keys from https://dev.twitter.com/apps
+  config.twitter_consumer_key = nuntium_config['twitter_consumer_key']
+  config.twitter_consumer_secret = nuntium_config['twitter_consumer_secret']
 end
 
