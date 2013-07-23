65e84ed69b363ed1ad82156cab947ee4e19471f7
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-07-23 19:03:22 -0300

    Support languages with ISO-639-3 codes

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -17,10 +17,6 @@
 
 module ProjectHelper
 
-  def languages_for_js
-    LanguageList::ISO_639_1.map{|l| {label: l.name, value: l.iso_639_1}}.to_json
-  end
-
   def project_languages_json(project)
     project.languages.map { |lang| {key: lang['language'], value: LanguageList::LanguageInfo.find(lang['language']).name} }.to_json
   end
