e95bbc432dd68b2ddc354ffcdf38d74cee4a0867
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-09-02 16:50:00 -0300

    Sort db records by name
    --HG--
    branch : new-broker

--- broker/include/db.hrl
+++ broker/include/db.hrl
@@ -1,19 +1,17 @@
--record(channel, {id, account_id, call_flow_id, name, config, type, created_at, updated_at}).
--record(call_log, {id, account_id, project_id, finished_at, direction, address,
-  state, created_at, updated_at, channel_id, started_at, schedule_id, not_before, call_flow_id, fail_reason}).
--record(call_log_entry, {id, call_id, severity, details, created_at, updated_at}).
 -record(call_flow, {id, callback_url, flow, project_id, store_in_fusion_tables, created_at, updated_at}).
--record(queued_call, {id, channel_id, call_log_id, address, callback_url, flow, call_flow_id, status_callback_url,
-  schedule_id, not_before, retries, project_id, time_zone, variables, session_id, callback_params, created_at, updated_at}).
--record(schedule, {id, name, retries, time_from, time_to, weekdays, project_id, created_at, updated_at}).
--record(project, {id, account_id, name, status_callback_url, default_language, languages, encrypted_config, created_at, updated_at}).
--record(resource, {id, name, project_id, guid, created_at, updated_at}).
--record(localized_resource, {id, language, text, recorded_audio, uploaded_audio, url, type, guid, resource_id, created_at, updated_at}).
+-record(call_log, {id, account_id, project_id, finished_at, direction, address, state, created_at, updated_at, channel_id, started_at, schedule_id, not_before, call_flow_id, fail_reason}).
+-record(call_log_entry, {id, call_id, severity, details, created_at, updated_at}).
+-record(channel, {id, account_id, call_flow_id, name, config, type, created_at, updated_at}).
 -record(contact, {id, project_id, anonymous, created_at, updated_at}).
 -record(contact_address, {id, address, contact_id, project_id, created_at, updated_at}).
--record(persisted_variable, {id, contact_id, implicit_key, project_variable_id, value, created_at, updated_at}).
--record(project_variable, {id, project_id, name, created_at, updated_at}).
--record(recorded_audio, {id, contact_id, call_log_id, key, description, created_at, updated_at}).
--record(external_service, {id, project_id, guid, global_settings, created_at, updated_at}).
--record(trace_record, {id, call_flow_id, call_id, step_name, step_id, result, created_at, updated_at}).
 -record(delayed_job, {id, handler, run_at, created_at, updated_at}).
+-record(external_service, {id, project_id, guid, global_settings, created_at, updated_at}).
+-record(localized_resource, {id, language, text, recorded_audio, uploaded_audio, url, type, guid, resource_id, created_at, updated_at}).
+-record(persisted_variable, {id, contact_id, implicit_key, project_variable_id, value, created_at, updated_at}).
+-record(project, {id, account_id, name, status_callback_url, default_language, languages, encrypted_config, created_at, updated_at}).
+-record(project_variable, {id, project_id, name, created_at, updated_at}).
+-record(queued_call, {id, channel_id, call_log_id, address, callback_url, flow, call_flow_id, status_callback_url, schedule_id, not_before, retries, project_id, time_zone, variables, session_id, callback_params, created_at, updated_at}).
+-record(recorded_audio, {id, contact_id, call_log_id, key, description, created_at, updated_at}).
+-record(resource, {id, name, project_id, guid, created_at, updated_at}).
+-record(schedule, {id, name, retries, time_from, time_to, weekdays, project_id, created_at, updated_at}).
+-record(trace_record, {id, call_flow_id, call_id, step_name, step_id, result, created_at, updated_at}).
