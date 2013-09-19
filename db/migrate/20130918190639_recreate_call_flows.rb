cf88297673a4eb74e005bfe3c951cf86d2710601
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-09-19 15:47:16 -0300

    Do not crash when recreating call flows

--- db/migrate/20130918190639_recreate_call_flows.rb
+++ db/migrate/20130918190639_recreate_call_flows.rb
@@ -1,8 +1,12 @@
 class RecreateCallFlows < ActiveRecord::Migration
   def up
     CallFlow.find_each do |call_flow|
-      call_flow.user_flow_will_change!
-      call_flow.save!
+      begin
+        call_flow.user_flow_will_change!
+        call_flow.save!
+      rescue Exception => ex
+        puts "Error while recreating flow #{call_flow.id}: #{ex.message}"
+      end
     end
   end
 
