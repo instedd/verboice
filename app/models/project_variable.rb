3b828a292438101066c95bb7889dbf3ee85f80f3
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-09-25 13:00:01 -0300

    Do not allow duplicate variable names (case insensitive)

--- app/models/project_variable.rb
+++ app/models/project_variable.rb
@@ -19,6 +19,6 @@ class ProjectVariable < ActiveRecord::Base
   belongs_to :project, :inverse_of => :project_variables
   has_many :persisted_variables, :dependent => :destroy, :inverse_of => :project_variable
   attr_accessible :name
-  validates_uniqueness_of :name, :scope => :project_id
+  validates_uniqueness_of :name, :scope => :project_id, :case_sensitive => false
   broker_cached
 end
