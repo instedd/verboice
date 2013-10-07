fcd05396de40cbab3d038a0c8b9b229c5758b474
Juan Wajnerman <jwajnerman@manas.com.ar>
2013-10-07 18:16:36 -0300

    Load encrypted config for call flows

--- broker/src/models/call_flow.erl
+++ broker/src/models/call_flow.erl
@@ -9,4 +9,11 @@ load_flow(CallFlow = #call_flow{callback_url = CallbackUrl, broker_flow = CompFl
     undefined -> flow:deserialize(CompFlow);
     _ -> [answer, [callback, [{url, binary_to_list(CallbackUrl)}]]]
   end,
-  CallFlow#call_flow{broker_flow = NewFlow}.
+  Config = case CallFlow#call_flow.encrypted_config of
+    undefined -> [];
+    CryptConfig ->
+      {ok, Secret} = application:get_env(verboice, crypt_secret),
+      [PlainConfig] = marshal:decode(aes:decrypt(Secret, base64:decode(CryptConfig))),
+      PlainConfig
+  end,
+  CallFlow#call_flow{broker_flow = NewFlow, encrypted_config = Config}.
