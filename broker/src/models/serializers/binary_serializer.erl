4a4589697d8e0679fd6f0fd999430bd8f23806f7
Carolina Hadad <chadad@manas.com.ar>
2014-03-10 16:11:56 -0300

    Added compression to the binary serializer

--- broker/src/models/serializers/binary_serializer.erl
+++ broker/src/models/serializers/binary_serializer.erl
@@ -2,7 +2,15 @@
 -export([load/1, dump/1]).
 
 load(Binary) ->
-  binary_to_term(Binary).
+  Z = zlib:open(),
+  zlib:inflateInit(Z),
+  Term = binary_to_term(iolist_to_binary(zlib:inflate(Z, binary_to_list(Binary)))),
+  zlib:close(Z),
+  Term.
 
 dump(Term) ->
-  term_to_binary(Term).
+  Z = zlib:open(),
+  zlib:deflateInit(Z),
+  CompTerm = iolist_to_binary(zlib:deflate(Z, term_to_binary(Term), full)),
+  zlib:close(Z),
+  CompTerm.
