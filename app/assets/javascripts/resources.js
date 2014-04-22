a1fea224637e12423d9e253e5a58db374bb7265f
Carolina Hadad <chadad@manas.com.ar>
2014-04-22 15:55:30 -0300

    Fixed Issue #353: Show alert when leaving resources page without saving

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -3,4 +3,11 @@
 onResources(function(){
   window.project = new Project();
   ko.applyBindings(project);
+  window.onbeforeunload = function() {
+    editing_any = _.some(project.resources(), function(res) { return res.editing() });
+    if (editing_any) {
+      return "If you leave this page without saving the resources you are editing your changes will be lost.  Are you sure you want to exit this page?";
+    };
+    return null;
+  };
 })
