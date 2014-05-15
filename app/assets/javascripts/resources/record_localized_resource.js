6bad2966c52bdd988722372b1408b8853edb3dcb
Juan Blandini <jblandini@manas.com.ar>
2014-05-15 16:10:51 -0300

    Removing commented lines and an unused handler

--- app/assets/javascripts/resources/record_localized_resource.js
+++ app/assets/javascripts/resources/record_localized_resource.js
@@ -44,34 +44,16 @@ onResources(function(){
       window.currentResource = currentResource;
       recorderElement.addEventListener("recorderStart", "RecordLocalizedResource.prototype.startHandler");
       recorderElement.addEventListener("recorderComplete", "RecordLocalizedResource.prototype.completeHandler");
-      recorderElement.addEventListener("playbackStart", "RecordLocalizedResource.prototype.playbackStartHandler");
       recorderElement.addEventListener("playbackComplete", "RecordLocalizedResource.prototype.playbackCompleteHandler");
       this.listenersInitialized = true;
     }
   }
-
-  // function uploadErrorHandler(info) {
-  //   console.log("Error " + info.errorId + " " + info.text);
-  // }
-
-  // function uploadProgressHandler(info) {
-  //   console.log("Progress" + info.bytesLoaded + "/" + info.bytesTotal);
-  // }
-
-  // function uploadCompleteHandler(info) {
-  //   console.log("Upload Complete")
-  //   console.log(info.data);
-  // }
 
   RecordLocalizedResource.prototype.startHandler = function(info) {
     window.currentResource.recording(true);
     window.currentResource.recordingStart = window.currentResource.nowSeconds();
     window.timeHandler = window.setInterval( function(){ return window.currentResource.updateDuration(window.currentResource.nowSeconds() - window.currentResource.recordingStart)}, 500 );
   }
-
-  // function progressHandler(info) {
-  //   console.log("Recorder Progress");
-  // }
 
   RecordLocalizedResource.prototype.completeHandler = function(info) {
     clearInterval(window.timeHandler);
@@ -80,62 +62,12 @@ onResources(function(){
     window.currentResource.hasAudio(true);
   }
 
-  // RecordLocalizedResource.prototype.playbackStartHandler = function(info) {
-  // }
-
   RecordLocalizedResource.prototype.playbackCompleteHandler = function(info) {
     window.currentResource.playing(false);
     clearInterval(window.currentResource.updateDurationInterval);
     window.currentResource.duration(window.currentResource.totalDuration);
   }
 
-  // function accessDeniedHandler(info) {
-  //   console.log("Access Denied");
-  // }
-
-  // function accessGrantedHandler(info) {
-  //   console.log("Access Granted");
-  // }
-
-  // function encoderErrorHandler(info) {
-  //   console.log("Encoder Error");
-  // }
-
-  // function playRaw2() {
-  //   document.getElementById("recorder").playRaw("http://verboice-stg.instedd.org/projects/541/resources/8775/localized_resources/8884/play_recording");
-  // }
-
-  // function playMp3() {
-  //   document.getElementById("recorder").playMp3("http://verboice-stg.instedd.org/projects/541/resources/8775/localized_resources/8884/play_recording");
-  // }
-
-  // function enabled() {
-  //   console.log(document.getElementById("recorder").enabled());
-  // }
-
-  // function uploadRaw() {
-  //   document.getElementById("recorder").uploadRaw(self.saveRecordingUrl());
-  // }
-
-  // function uploadMp3() {
-  //   document.getElementById("recorder").uploadMp3("http://verboice-stg.instedd.org/projects/541/resources/8775/localized_resources/8884/save_recording");
-  // }
-
-  // function gain() {
-  //   document.getElementById("recorder").gain(200);
-  // }
-
-  // function rate() {
-  //   document.getElementById("recorder").rate(11);
-  // }
-
-  // function silentLevel() {
-  //   document.getElementById("recorder").silentLevel(10);
-  // }
-
-  // function timeOut() {
-  //   document.getElementById("recorder").timeOut(100);
-  // }
 
   RecordLocalizedResource.prototype.stop= function(){
     document.getElementById("recorder").stop();
