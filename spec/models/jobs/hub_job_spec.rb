61cfea2ca98ddac679b22c0d0a81603a70d3d168
Mariano Abel Coca <marianoabelcoca@gmail.com>
2014-11-19 15:18:45 -0300

    Updated hub_client gem.

--- spec/models/jobs/hub_job_spec.rb
+++ spec/models/jobs/hub_job_spec.rb
@@ -10,7 +10,9 @@ describe Jobs::HubJob do
     project_var = project.project_variables.make name: "var1"
     persisted_var = PersistedVariable.make contact_id: contact.id, project_variable_id: project_var.id, value: "value1"
 
-    stub_request(:post, "http://hub.instedd.org/callback/verboice/call_finished?token=some_token").
+    HubClient.current token: "some_token"
+
+    stub_request(:post, "https://hub.instedd.org/callback/verboice/call_finished?token=some_token").
       with(:body => "{\"project_id\":#{call_log.project_id},\"call_flow_id\":#{call_log.call_flow_id},\"address\":\"#{address}\",\"vars\":{\"var1\":\"value1\"}}").
       to_return(:status => 200, :body => "", :headers => {})
 
