acef71b2c28aa0df7d90415b3c41c6537834d0e9
Adrian Romero <aromero@manas.com.ar>
2015-01-26 14:32:38 -0300

    #635: Fixed issues while importing a call flow with external services (global settings in external service model, response type in external service step model)

--- app/models/external_service.rb
+++ app/models/external_service.rb
@@ -21,7 +21,7 @@ class ExternalService < ActiveRecord::Base
   has_many :call_flow_external_services, :dependent => :destroy
   has_many :call_flows, :through => :call_flow_external_services
 
-  attr_accessible :name, :url, :xml, :global_variables_attributes, :guid
+  attr_accessible :name, :url, :xml, :global_variables_attributes, :guid, :global_settings
 
   validates :guid, :presence => true, :uniqueness => { :scope => :project_id }
 
