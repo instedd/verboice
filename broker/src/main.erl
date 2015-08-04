1132dcd11436e41dc8d564b4cba97c67cc8294c9
Santiago Palladino <spalladino@gmail.com>
2015-08-04 15:31:07 -0300

    Force sanity check to start up broker to prevent setup errors to arise when performing calls

--- broker/src/main.erl
+++ broker/src/main.erl
@@ -2,5 +2,11 @@
 -export([start/0]).
 
 start() ->
-  application:ensure_all_started(verboice, permanent),
-  application:ensure_all_started(ernie_server, permanent).
+  application:load(verboice),
+  case sanity_check:check_all() of
+    {error, Reason} ->
+      error({"Startup check failed", Reason});
+    ok ->
+      application:ensure_all_started(verboice, permanent),
+      application:ensure_all_started(ernie_server, permanent)
+  end.
