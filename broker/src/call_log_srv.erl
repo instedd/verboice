eb74a8df55c1fd851445129be40c4de0fdac05dd
Santiago Palladino <spalladino@gmail.com>
2015-08-05 15:19:21 -0300

    Fixes #708. Do not kill call_log_srv as soon as session dies, but wait 500ms so pbx_guid can be associated by the asterisk pbx log srv.

--- broker/src/call_log_srv.erl
+++ broker/src/call_log_srv.erl
@@ -5,7 +5,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
--record(state, {call_log, owner_pid}).
+-record(state, {call_log, owner_pid, timeout}).
 
 -include("db.hrl").
 
@@ -28,11 +28,11 @@ trace_record(CallFlowId, StepId, StepName, Result, {?MODULE, Pid}) ->
 update(Fields, {?MODULE, Pid}) ->
   gen_server:cast(Pid, {update, Fields}).
 
-id({?MODULE, Pid}) ->
-  gen_server:call(Pid, get_id).
-
 associate_pbx_log(SessionId, PbxLogId) ->
   gen_server:cast({global, {call_log_srv, SessionId}}, {associate_pbx_log, PbxLogId}).
+
+id({?MODULE, Pid}) ->
+  gen_server:call(Pid, get_id).
 
 %% @private
 init({CallLog, Owner}) ->
@@ -41,27 +41,27 @@ init({CallLog, Owner}) ->
     undefined -> gen_server:cast(self(), create);
     _ -> ok
   end,
-  {ok, #state{call_log = CallLog, owner_pid = Owner}}.
+  {ok, #state{call_log = CallLog, owner_pid = Owner, timeout = infinity}}.
 
 %% @private
-handle_call(get_id, _From, State = #state{call_log = CallLog}) ->
-  {reply, CallLog#call_log.id, State}.
+handle_call(get_id, _From, State = #state{call_log = CallLog, timeout = Timeout}) ->
+  {reply, CallLog#call_log.id, State, Timeout}.
 
 %% @private
-handle_cast(create, State = #state{call_log = CallLog}) ->
+handle_cast(create, State = #state{call_log = CallLog, timeout = Timeout}) ->
   NewCallLog = CallLog:create(),
-  {noreply, State#state{call_log = NewCallLog}};
+  {noreply, State#state{call_log = NewCallLog}, Timeout};
 
-handle_cast({log, Level, Message, Details}, State = #state{call_log = CallLog}) ->
+handle_cast({log, Level, Message, Details}, State = #state{call_log = CallLog, timeout = Timeout}) ->
   % CallLog:Level(Message, Details),
   call_log_entry_srv:log(CallLog#call_log.id, Level, Message, Details),
-  {noreply, State};
+  {noreply, State, Timeout};
 
-handle_cast({update, Fields}, State = #state{call_log = CallLog}) ->
+handle_cast({update, Fields}, State = #state{call_log = CallLog, timeout = Timeout}) ->
   NewCallLog = CallLog:update(Fields),
-  {noreply, State#state{call_log = NewCallLog}};
+  {noreply, State#state{call_log = NewCallLog}, Timeout};
 
-handle_cast({trace_record, CallFlowId, StepId, StepName, Result}, State = #state{call_log = CallLog}) ->
+handle_cast({trace_record, CallFlowId, StepId, StepName, Result}, State = #state{call_log = CallLog, timeout = Timeout}) ->
   % TraceRecord = #trace_record{
   %   call_flow_id = CallFlowId,
   %   step_id = StepId,
@@ -71,14 +71,18 @@ handle_cast({trace_record, CallFlowId, StepId, StepName, Result}, State = #state
   % },
   % TraceRecord:save(),
   call_log_entry_srv:trace(CallLog#call_log.id, CallFlowId, StepId, StepName, Result),
-  {noreply, State};
+  {noreply, State, Timeout};
 
-handle_cast({associate_pbx_log, PbxLogId}, State = #state{call_log = CallLog}) ->
+handle_cast({associate_pbx_log, PbxLogId}, State = #state{call_log = CallLog, timeout = Timeout}) ->
   NewCallLog = call_log:update(CallLog#call_log{pbx_logs_guid = PbxLogId}),
-  {noreply, State#state{call_log = NewCallLog}}.
+  {noreply, State#state{call_log = NewCallLog}, Timeout}.
 
 %% @private
 handle_info({'DOWN', _Ref, process, Pid, _Reason}, State = #state{owner_pid = Pid}) ->
+  % Wait for 500 milliseconds before actually stopping, to receive any pending updates (see issue 708)
+  {noreply, State#state{timeout = 500}, 500};
+
+handle_info(timeout, State) ->
   {stop, normal, State};
 
 handle_info(_Info, State) ->
