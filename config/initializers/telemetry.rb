b6445207699e17de1bf237830ca41682d3960be9
Adrian Romero <aromero@manas.com.ar>
2015-10-30 17:28:17 -0300

    Telemetry: changed configuration to read period size in hours

--- config/initializers/telemetry.rb
+++ config/initializers/telemetry.rb
@@ -3,7 +3,7 @@ InsteddTelemetry.setup do |conf|
   custom_config = Rails.configuration.telemetry_configuration rescue {}
 
   conf.server_url           = custom_config[:server_url]                   if custom_config.include? :server_url
-  conf.period_size          = custom_config[:period_size_days].days        if custom_config.include? :period_size_days
+  conf.period_size          = custom_config[:period_size_hours].hours      if custom_config.include? :period_size_hours
   conf.process_run_interval = custom_config[:run_interval_minutes].minutes if custom_config.include? :run_interval_minutes
 
   conf.remote_api_enabled = true
