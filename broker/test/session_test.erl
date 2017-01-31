1fbf6d6d0b272fb6cecf231f06d6336c544625c5
Gustavo Giráldez <ggiraldez@manas.com.ar>
2017-01-31 18:35:06 -0300

    Fix #797: Merge query string params when performing status callback
    When performing the status callback for a call, preserve the query string
    parameters which may have been present in the configured URL, and merge the call
    and session variables.

--- broker/test/session_test.erl
+++ broker/test/session_test.erl
@@ -77,3 +77,16 @@ notify_status_on_completed_with_session_vars_test() ->
 
   meck:wait(httpc, request, RequestParams, 1000),
   meck:unload().
+
+notify_status_merges_query_string_test() ->
+  Session = #session{address = <<"123">>, call_log = {call_log_srv}, status_callback_url = <<"http://foo.com?sample=1">>},
+  meck:new(call_log_srv, [stub_all]),
+  meck:expect(call_log_srv, id, 1, 1),
+  meck:new(httpc),
+
+  RequestParams = [get, {"http://foo.com/?sample=1&CallSid=1&CallStatus=completed&From=123&CallDuration=0", []}, '_', [{full_result, false}]],
+  meck:expect(httpc, request, RequestParams, ok),
+  session:in_progress({completed, Session, ok}, #state{session = Session}),
+
+  meck:wait(httpc, request, RequestParams, 1000),
+  meck:unload().
