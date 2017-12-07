d7cd438223966e64bbc72be8eb63c730c6e41366
Martin Verzilli <mverzilli@manas.tech>
2017-12-07 14:53:21 -0300

    Add Zeus to dev container

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@
 # along with Verboice.  If not, see <http://www.gnu.org/licenses/>.
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
