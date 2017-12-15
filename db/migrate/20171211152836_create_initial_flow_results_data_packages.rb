228a89d14c0784115c3f9dc682abd6bcea0eaed0
Martin Verzilli <mverzilli@manas.tech>
2017-12-15 15:24:04 -0300

    Make data packages retrocompatible with call logs corresponding to current version of each call flow

--- db/migrate/20171211152836_create_initial_flow_results_data_packages.rb
+++ db/migrate/20171211152836_create_initial_flow_results_data_packages.rb
@@ -8,7 +8,7 @@ class CreateInitialFlowResultsDataPackages < ActiveRecord::Migration
         flow_results_data_packages.until,
         flow_results_data_packages.created_at,
         flow_results_data_packages.updated_at)
-        SELECT UUID(), c.id, NOW(), NULL, NOW(), NOW()
+        SELECT UUID(), c.id, c.updated_at, NULL, NOW(), NOW()
         FROM call_flows c
         WHERE c.mode = 'flow'
         AND NOT EXISTS (SELECT 1 FROM flow_results_data_packages f WHERE f.call_flow_id = c.id AND f.until IS NULL)
