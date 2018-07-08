a090279715df695c8f112b1a5f3df93997c67901
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2018-07-07 21:07:47 -0300

    #824. Handle the user_hangup case in africas_talking_httpd

--- broker/src/africas_talking/africas_talking_httpd_module.erl
+++ broker/src/africas_talking/africas_talking_httpd_module.erl
@@ -7,42 +7,47 @@
 
 do(#mod{request_uri = "/africas_talking/" ++ _, method = "POST", entity_body = Body, data = Data}) ->
   Params = uri:parse_qs(Body),
+  CallSid = proplists:get_value("sessionId", Params),
+  Pbx = africas_talking_pbx:find(CallSid),
   case proplists:get_value("callSessionState", Params) of
     undefined ->
       httpd_utils:if_not_already_handled(Data, fun() ->
-        CallSid = proplists:get_value("sessionId", Params),
-
-        ResponseBody = case africas_talking_pbx:find(CallSid) of
+        ResponseBody = case Pbx of
           undefined ->
-            Pbx = africas_talking_pbx:new(CallSid),
+            NewPbx = africas_talking_pbx:new(CallSid),
             case africas_talking_sid:find_session_pid(CallSid) of
               undefined ->
                 Number = util:normalize_phone_number(proplists:get_value("destinationNumber", Params)),
                 CallerId = util:normalize_phone_number(proplists:get_value("callerNumber", Params)),
-                % The destinationNumber only is used to fetch the channel
+                % Channel is fetched by using just the destinationNumber (Inbound call)
                 Channel = africas_talking_channel_srv:find_channel(Number),
 
                 {ok, SessionPid} = session:new(),
                 africas_talking_sid:start(CallSid, SessionPid),
 
                 % FIX this may lead to a race condition if the session reaches a flush before the resume is called
-                session:answer(SessionPid, Pbx, Channel#channel.id, CallerId);
+                session:answer(SessionPid, NewPbx, Channel#channel.id, CallerId);
               SessionPid ->
-                session:answer(SessionPid, Pbx)
+                session:answer(SessionPid, NewPbx)
             end,
 
-            Pbx:resume(Params);
-          Pbx ->
-            CallStatus = proplists:get_value("CallStatus", Params),
-            case CallStatus of
-              "completed" -> Pbx:user_hangup(), "OK";
-              _ -> Pbx:resume(Params)
-            end
-        end,
-
-        Response = [{response, {200, ResponseBody}}],
-        {proceed, Response}
-      end);
+            NewPbx:resume(Params);
+          FoundPbx ->
+            FoundPbx:resume(Params)
+          end,
+          Response = [{response, {200, ResponseBody}}],
+          {proceed, Response}
+        end);
+    % TODO. If the call fails and the session is still active, a user_hangup is logged,
+    % which is not true. Handle those cases properly.
+    "Completed" ->
+      case Pbx of
+        undefined ->
+          ok;
+        FoundPbx ->
+          FoundPbx:user_hangup()
+      end,
+      {proceed, [{response, {200, "OK"}}]};
     _ ->
       lager:info("Receiving callSessionState ~p", [Params]),
       {proceed, [{response, {200, "OK"}}]}
