a6cb31aac4b6ea62c96381998df354c018c41c06
Juan Wajnerman <jwajnerman@manas.com.ar>
2019-04-16 14:20:16 -0300

    Allow Pigeon configuration through environment variables (Nuntium config)

--- config/initializers/pigeon.rb
+++ config/initializers/pigeon.rb
@@ -4,14 +4,13 @@ nuntium_config = File.exists?(nuntium_config_path) ? YAML::load(File.open(nuntiu
 Pigeon.setup do |config|
   config.application_name = 'Verboice'
 
-  config.nuntium_host = nuntium_config['host']
-  config.nuntium_account = nuntium_config['account']
-  config.nuntium_app = nuntium_config['application']
-  config.nuntium_app_password = nuntium_config['password']
+  config.nuntium_host = ENV['NUNTIUM_HOST'] || nuntium_config['host']
+  config.nuntium_account = ENV['NUNTIUM_ACCOUNT'] || nuntium_config['account']
+  config.nuntium_app = ENV['NUNTIUM_APP'] || nuntium_config['application']
+  config.nuntium_app_password = ENV['NUNTIUM_APP_PASSWORD'] || nuntium_config['password']
 
   # If you want to support Nuntium Twitter channels, get your Twitter
   # consumer keys from https://dev.twitter.com/apps
   config.twitter_consumer_key = nuntium_config['twitter_consumer_key']
   config.twitter_consumer_secret = nuntium_config['twitter_consumer_secret']
 end
-
