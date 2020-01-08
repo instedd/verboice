b00d44a2e23d20f1196cc6f6a562563294b948c3
Andrés Atencio <aatencio@manas.tech>
2020-01-08 13:35:53 -0300

    Workaround for occasional failure in dev-setup

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 docker-compose build
+
+# The next two commands occasionally fail. If so, replace them with:
+# docker-compose run --rm broker bash
+#   ./rebar get-deps
+#   ./rebar compile
 docker-compose run --rm broker ./rebar get-deps
 docker-compose run --rm broker ./rebar compile
+
 docker-compose run --rm web bundle install
 docker-compose run --rm web bundle exec rake db:setup
 docker-compose run --rm web bundle exec rake db:test:prepare
