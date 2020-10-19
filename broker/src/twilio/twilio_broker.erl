adb8a971b16e53d3f6dd534fbc3073a8a39afabd
aeatencio <aeatencio@gmail.com>
2020-10-19 15:02:12 -0300

    De-hardcode Twilio base URL
    It will be used for defaulting the new setting
    
    Refactor for #878

--- broker/src/twilio/twilio_broker.erl
+++ broker/src/twilio/twilio_broker.erl
@@ -28,7 +28,7 @@ dispatch(_Session = #session{session_id = SessionId, channel = Channel, address 
   CallbackUrl = verboice_config:twilio_callback_url(),
   CallbackUri = uri:parse(CallbackUrl),
 
-  RequestUrl = ["https://api.twilio.com/2010-04-01/Accounts/", AccountSid, "/Calls"],
+  RequestUrl = [verboice_config:twilio_base_url() ++ "/Accounts/", AccountSid, "/Calls"],
   RequestBody = [
     {'From', util:normalize_phone_number(channel:number(Channel))},
     {'To', util:normalize_phone_number(Address)},
