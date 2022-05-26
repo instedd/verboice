7260d80f8af378faba2c6f9c3560687b631e19ad
aeatencio <aatencio@manas.tech>
2022-05-26 13:39:10 -0300

    Make it simpler
    See https://github.com/instedd/verboice/pull/912#discussion_r882740718

--- broker/src/scheduler/active_calls_gc.erl
+++ broker/src/scheduler/active_calls_gc.erl
@@ -10,14 +10,12 @@
 
 -define(SERVER, ?MODULE).
 
--define(MILLISECS_IN_ONE_MINUTE, 60000).
--define(INIT_DELAY, ?MILLISECS_IN_ONE_MINUTE).
-
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, {}, []).
 
 init({}) ->
-  erlang:send_after(?INIT_DELAY,self(),cancel_active_calls),
+  Interval = verboice_config:minutes_between_active_calls_gc_runs(),
+  timer:send_interval(timer:minutes(Interval), cancel_active_calls),
   {ok, undefined}.
 
 handle_call(_Request, _From, State) ->
@@ -31,8 +29,6 @@ handle_info(cancel_active_calls, State) ->
   N_Minutes_Ago = util:seconds_ago(N * 60),
   Count = call_log:cancel_active_calls_started_before(N_Minutes_Ago),
   lager:info("GC cancelled ~p calls that stayed active for more than ~p minutes", [Count, N]),
-  Interval = ?MILLISECS_IN_ONE_MINUTE * verboice_config:minutes_between_active_calls_gc_runs(),
-  erlang:send_after(Interval, self(), cancel_active_calls),
   {noreply, State};
 
 handle_info(_Info, State) ->
