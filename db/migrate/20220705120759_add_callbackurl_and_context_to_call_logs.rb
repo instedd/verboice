bb5ef8189b6a1a6a9d15c964e41508197b4067b7
leandroradusky <leandro.radusky@gmail.com>
2022-07-06 19:38:25 +0200

    Delete 20220705120759_add_callbackurl_and_context_to_call_logs.rb

--- db/migrate/20220705120759_add_callbackurl_and_context_to_call_logs.rb
+++
@@ -1,7 +0,0 @@
-class AddCallbackUrlAndJsContextToCallLogs < ActiveRecord::Migration
-    def change
-        add_column :call_logs, :callback_url, :string, default: ""
-        add_column :call_logs, :js_context, :text, default: ""
-    end
-  end
-  
